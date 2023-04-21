7814619e1f2198763ae1a25c486ef0427ce99d62
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-21 17:43:11 -0300

    images e dependencias

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import Banner from './images/banner.png';
+
+const Title = styled.h1`
+font-family: 'Prata';
+font-style: normal;
+font-weight: 400;
+font-size: ${prop => prop.fontSize || '32px'} ;
+line-height: 43px;
+display: flex;
+align-items: center;
+text-align: center;
+color: #212121;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+
+     <section><Title fontSize="40px">Minha Organização:</Title></section>
+     <section><Title>Programação</Title></section>
+     <section><Title>Front End</Title></section>
+     <section><Title>Date SCience</Title></section>
+     <section><Title>Devops</Title></section>
+     <section><Title>UX e Design</Title></section>
+     <section><Title>Mobile</Title></section>
+     <section><Title>Inovação e Gestão </Title></section>
+     <footer>footer</footer>
+
     </div>
   );
 }
 
 export default App;
+function Header() {
+  return <header><img src={Banner} alt="banner" /></header>;
+}
+
