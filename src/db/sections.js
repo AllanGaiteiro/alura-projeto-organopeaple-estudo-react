5cc859666996df7c145a7a11f99624aca29ff1b0
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-21 18:22:28 -0300

    criando base de dados das sections

---
+++ src/db/sections.js
@@ -0,0 +1,37 @@
+export const sections = [
+  {
+    id: "programming-section",
+    backgroundColor: "#D9F7E9",
+    title: "Programação",
+  },
+  {
+    id: "front-end-section",
+    backgroundColor: "#E8FFFF",
+    title: "Front End",
+  },
+  {
+    id: "data-science-section",
+    backgroundColor: "#E9FFE3",
+    title: "Data Science",
+  },
+  {
+    id: "devops-section",
+    backgroundColor: "rgba(241, 97, 101, 0.15)",
+    title: "Devops",
+  },
+  {
+    id: "ux-design-section",
+    backgroundColor: "rgba(220, 110, 190, 0.15)",
+    title: "UX e Design",
+  },
+  {
+    id: "mobile-section",
+    backgroundColor: "rgba(255, 186, 5, 0.15)",
+    title: "Mobile",
+  },
+  {
+    id: "innovation-management-section",
+    backgroundColor: "rgba(255, 140, 42, 0.15)",
+    title: "Inovação e Gestão",
+  },
+];
