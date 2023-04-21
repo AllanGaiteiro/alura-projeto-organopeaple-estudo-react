6ef1cb67f2a78a7be88b2455bcc69419d99480e2
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-21 18:49:04 -0300

    refactor dos components de section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,18 @@
+import { CardEmployer } from "./CardEmployer";
+import { CardList } from "./CardList";
 import { SectionContainer } from "./SectionContainer";
 import { Title } from "./Title";
 
-export function Section({ backgroundColor, title }) {
+export function Section({ section }) {
+  const { backgroundColor, title, cardCollor } = section;
   return (
     <SectionContainer backgroundColor={backgroundColor}>
       <Title>{title}</Title>
+      <CardList>
+        <CardEmployer cardCollor={cardCollor}></CardEmployer>
+        <CardEmployer cardCollor={cardCollor}></CardEmployer>
+        <CardEmployer cardCollor={cardCollor}></CardEmployer>
+      </CardList>
     </SectionContainer>
   );
 }
