c9d689bfba060327d7a5d900a0fc61d252b1c1b8
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-21 21:57:07 -0300

    tirando borda

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,7 +5,9 @@ export const Input = styled.input`
   width: 930px;
   left: 255px;
   top: 1193px;
+  border: none;
   border-radius: 0px;
+
   mix-blend-mode: normal;
   box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
   border-radius: 0px;
