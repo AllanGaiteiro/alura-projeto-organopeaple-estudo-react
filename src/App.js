7907a804a1c303a58db2a193939db7bf5e05306b
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-22 10:14:18 -0300

    criação do App container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./App.css";
+import styled from "styled-components";
 import { Header } from "./components/Header";
 import { SectionSignEmployers } from "./components/SectionSignEmployers";
 import { Sections } from "./components/Sections";
 import { Sidebar } from "./components/Sidebar";
-import { SidebarButton } from "./components/SidebarButton";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar>
-        <SidebarButton>1</SidebarButton>
-        <SidebarButton>2</SidebarButton>
-        <SidebarButton>3</SidebarButton>
-      </Sidebar>
+    <AppContainer>
+      <Sidebar></Sidebar>
       <Header />
       <SectionSignEmployers />
       <Sections />
 
       <footer>footer</footer>
-    </div>
+    </AppContainer>
   );
 }
 
